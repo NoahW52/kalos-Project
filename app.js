923a20b42ff3b5376eec4ca760a8fdcfbc6a3ecd
Noah Wright <nwright5252@gmail.com>
2023-03-30 17:24:55 -0500

    navbar and css

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const mustacheExpress = require('mustache-express')
-const bcrypt = require('bcrypt.js')
+
 const mongoose = require('mongoose')
-let salt = await bcrypt.genSalt(10)
-let hashedPassword = awaitbcrypt.hash
+
+app.use('/css/version-1', express.static('css'))
 
 mongoose.connect('mongodb+srv://curranod840:zSfsG5c5SeoIaPpT@cluster.8cz7y6f.mongodb.net/?retryWrites=true&w=majority',
 {
@@ -15,17 +15,14 @@ mongoose.connect('mongodb+srv://curranod840:zSfsG5c5SeoIaPpT@cluster.8cz7y6f.mon
 }).catch((error) => {
     console.log(error)
 })
-
-app.use(session({
-    secret: 'sksksksks',
-    resave: false,
-    saveUninitialized: true
-}))
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
 app.use(express.urlencoded())
 
+app.get('/', (req, res) => {
+    res.render('display')
+})
 
 app.listen(8080, () => {
     console.log('server is running on http://localhost:8080')
