d6bc61a191648efc862b0eed31c935d941a90c8f
curranod <curranod840@gmail.com>
2023-03-31 11:58:33 -0500

    LOGIN COMPLETE

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const app = express()
 const mustacheExpress = require('mustache-express')
-// const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs')
 const mongoose = require('mongoose')
 const User = require('./schemas/user')
-// let salt = await bcrypt.genSalt(10)
-// let hashedPassword = awaitbcrypt.hash
 require('dotenv').config()
 const session = require('express-session')
 
-mongoose.connect('mongodb+srv://curranod840:zSfsG5c5SeoIaPpT@cluster.8cz7y6f.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect(`mongodb+srv://curranod840:${process.env.DB_PASSWORD}@cluster.8cz7y6f.mongodb.net/?retryWrites=true&w=majority`,
 {
     useNewUrlParser: true,
 })
@@ -38,16 +36,18 @@ app.get('/home', (req, res) => {
     res.render('home')
 })
 
-
-
 app.get('/register', async(req, res) => {
     res.render('register')
 })
 
 
 app.post('/register', async(req, res) => {
+    const password = req.body.password
+    let salt = await bcrypt.genSalt(10)
+    let hashedPassword = await bcrypt.hash(password, salt)
     const user = new User({
         username: req.body.username,
+        password: hashedPassword,
         email: req.body.email,
         phone: req.body.phone,
         firstName: req.body.firstName,
@@ -57,6 +57,36 @@ app.post('/register', async(req, res) => {
     res.redirect('/home')
 })
 
+app.get('/login', async(req, res) => {
+    res.render('login')
+})
+
+app.post('/login', async(req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    const user = await User.findOne({username: username})
+    const hashedPassword = user.password
+
+    if(user) {
+        // compare passwords 
+        const result = await bcrypt.compare(password, hashedPassword)
+        if(result) {
+            // user has been authenticated 
+            // put something in the session 
+            if(req.session) {
+                req.session.userId = user.id 
+            }
+
+            // send them to the home screen 
+            res.redirect('/home')
+        } else {
+            res.render('login', { errorMessage: 'Invalid credentials.'})
+        }
+    } else {
+        res.render('login', { errorMessage: 'no'})
+    }
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`server is running on http://localhost:${process.env.PORT}`)
